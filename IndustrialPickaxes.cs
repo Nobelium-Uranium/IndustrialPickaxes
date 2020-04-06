94760ace4c7b388f96b7cd35a83adde882b1ff43
Chem <minecartanimation@gmail.com>
2020-04-06 07:30:41 -0600

    Manipulator UI (AKA AoE mining UI) TODO: Fix AoE smelting

--- IndustrialPickaxes.cs
+++ IndustrialPickaxes.cs
@@ -5,6 +5,10 @@ using Terraria;
 using Terraria.ID;
 using Terraria.Localization;
 using Terraria.ModLoader;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using Terraria.UI;
 
 namespace IndustrialPickaxes
 {
@@ -71,97 +75,128 @@ namespace IndustrialPickaxes
 			Veinminer = null;
 		}
 
-		public override void AddRecipeGroups()
-		{
-			RecipeGroup group = new RecipeGroup(() => Language.GetTextValue("LegacyMisc.37") + " Large Gem", new int[]
-			{
-				ItemID.LargeDiamond,
-				ItemID.LargeTopaz,
-				ItemID.LargeRuby,
-				ItemID.LargeSapphire,
-				ItemID.LargeEmerald,
-				ItemID.LargeAmethyst,
-				ItemID.LargeAmber,
-			});
+        public override void AddRecipeGroups()
+        {
+            RecipeGroup group = new RecipeGroup(() => Language.GetTextValue("LegacyMisc.37") + " Large Gem", new int[]
+            {
+                ItemID.LargeDiamond,
+                ItemID.LargeTopaz,
+                ItemID.LargeRuby,
+                ItemID.LargeSapphire,
+                ItemID.LargeEmerald,
+                ItemID.LargeAmethyst,
+                ItemID.LargeAmber,
+            });
 
-			RecipeGroup.RegisterGroup("IndustrialPickaxes:LargeGem", group);
+            RecipeGroup.RegisterGroup("IndustrialPickaxes:LargeGem", group);
 
-			group = new RecipeGroup(() => "Fishaxe", new int[]
-			{
-				ModContent.ItemType<Fishaxe>(),
-				ModContent.ItemType<FishaxeRitual>(),
-			});
+            group = new RecipeGroup(() => "Fishaxe", new int[]
+            {
+                ModContent.ItemType<Fishaxe>(),
+                ModContent.ItemType<FishaxeRitual>(),
+            });
 
-			RecipeGroup.RegisterGroup("IndustrialPickaxes:Fishaxe", group);
+            RecipeGroup.RegisterGroup("IndustrialPickaxes:Fishaxe", group);
 
-			group = new RecipeGroup(() => "Blazing Molten Pickaxe", new int[]
-			{
-				ModContent.ItemType<IndustrialMoltenPickaxe>(),
-				ModContent.ItemType<IndustrialMoltenPickaxeFrostburn>(),
-			});
+            group = new RecipeGroup(() => "Blazing Molten Pickaxe", new int[]
+            {
+                ModContent.ItemType<IndustrialMoltenPickaxe>(),
+                ModContent.ItemType<IndustrialMoltenPickaxeFrostburn>(),
+            });
 
-			RecipeGroup.RegisterGroup("IndustrialPickaxes:IndustrialMolten", group);
+            RecipeGroup.RegisterGroup("IndustrialPickaxes:IndustrialMolten", group);
 
-			group = new RecipeGroup(() => "Graceful Chlorophyte Pickaxe", new int[]
-			{
-				ModContent.ItemType<GracefulChlorophytePickaxe>(),
-				ModContent.ItemType<GracefulChlorophytePickaxePlantera>(),
-			});
+            group = new RecipeGroup(() => "Graceful Chlorophyte Pickaxe", new int[]
+            {
+                ModContent.ItemType<GracefulChlorophytePickaxe>(),
+                ModContent.ItemType<GracefulChlorophytePickaxePlantera>(),
+            });
 
-			RecipeGroup.RegisterGroup("IndustrialPickaxes:IndustrialChlorophyte", group);
+            RecipeGroup.RegisterGroup("IndustrialPickaxes:IndustrialChlorophyte", group);
 
-			group = new RecipeGroup(() => "Lihzahrdian Picksaw", new int[]
-			{
-				ModContent.ItemType<IndustrialPicksaw>(),
-				ModContent.ItemType<IndustrialPicksawBysmal>(),
-				ModContent.ItemType<IndustrialPicksawMartian>(),
-			});
+            group = new RecipeGroup(() => "Lihzahrdian Picksaw", new int[]
+            {
+                ModContent.ItemType<IndustrialPicksaw>(),
+                ModContent.ItemType<IndustrialPicksawBysmal>(),
+                ModContent.ItemType<IndustrialPicksawMartian>(),
+            });
 
-			RecipeGroup.RegisterGroup("IndustrialPickaxes:IndustrialPicksaw", group);
+            RecipeGroup.RegisterGroup("IndustrialPickaxes:IndustrialPicksaw", group);
 
-			group = new RecipeGroup(() => "Lunatic's Celestial Pick", new int[]
-			{
-				ModContent.ItemType<IndustrialLunarPickaxe>(),
-				ModContent.ItemType<IndustrialLunarPickaxeRagnarok>(),
-			});
+            group = new RecipeGroup(() => "Lunatic's Celestial Pick", new int[]
+            {
+                ModContent.ItemType<IndustrialLunarPickaxe>(),
+                ModContent.ItemType<IndustrialLunarPickaxeRagnarok>(),
+            });
 
-			RecipeGroup.RegisterGroup("IndustrialPickaxes:IndustrialLunar", group);
+            RecipeGroup.RegisterGroup("IndustrialPickaxes:IndustrialLunar", group);
 
-			group = new RecipeGroup(() => "Means to an End", new int[]
-			{
-				ModContent.ItemType<DraconicStaffOfPower>(),
-				ModContent.ItemType<DraconicStaffOfPowerAsiimov>(),
-				ModContent.ItemType<DraconicStaffOfPowerDemonic>(),
-				ModContent.ItemType<DraconicStaffOfPowerGlacier>(),
-			});
+            group = new RecipeGroup(() => "Means to an End", new int[]
+            {
+                ModContent.ItemType<DraconicStaffOfPower>(),
+                ModContent.ItemType<DraconicStaffOfPowerAsiimov>(),
+                ModContent.ItemType<DraconicStaffOfPowerDemonic>(),
+                ModContent.ItemType<DraconicStaffOfPowerGlacier>(),
+            });
 
-			RecipeGroup.RegisterGroup("IndustrialPickaxes:DraconicEvolution", group);
+            RecipeGroup.RegisterGroup("IndustrialPickaxes:DraconicEvolution", group);
 
-			group = new RecipeGroup(() => "Solus Flarium Pickaxe", new int[]
-			{
-				ModContent.ItemType<SolusFlariumPickaxe>(),
-				ModContent.ItemType<SolusFlariumPickaxeCernium>(),
-				ModContent.ItemType<SolusFlariumPickaxeAsthraltite>(),
-			});
+            group = new RecipeGroup(() => "Solus Flarium Pickaxe", new int[]
+            {
+                ModContent.ItemType<SolusFlariumPickaxe>(),
+                ModContent.ItemType<SolusFlariumPickaxeCernium>(),
+                ModContent.ItemType<SolusFlariumPickaxeAsthraltite>(),
+            });
 
-			RecipeGroup.RegisterGroup("IndustrialPickaxes:IndustrialFlarium", group);
+            RecipeGroup.RegisterGroup("IndustrialPickaxes:IndustrialFlarium", group);
 
-			group = new RecipeGroup(() => "Viridescent Blossom Pickaxe", new int[]
-			{
-				ModContent.ItemType<ViridescentBlossomPickaxe>(),
-				ModContent.ItemType<IndustrialBlossomPickaxeProvidence>(),
-			});
+            group = new RecipeGroup(() => "Viridescent Blossom Pickaxe", new int[]
+            {
+                ModContent.ItemType<ViridescentBlossomPickaxe>(),
+                ModContent.ItemType<IndustrialBlossomPickaxeProvidence>(),
+            });
 
-			RecipeGroup.RegisterGroup("IndustrialPickaxes:IndustrialBlossom", group);
+            RecipeGroup.RegisterGroup("IndustrialPickaxes:IndustrialBlossom", group);
 
-			group = new RecipeGroup(() => "Experimental Nano Pickaxe", new int[]
-			{
-				ModContent.ItemType<ExperimentalNanoPickaxe>(),
-				ModContent.ItemType<ExperimentalNanoPickaxeCreative>(),
-				ModContent.ItemType<ExperimentalNanoPickaxeXenium>(),
-			});
+            group = new RecipeGroup(() => "Experimental Nano Pickaxe", new int[]
+            {
+                ModContent.ItemType<ExperimentalNanoPickaxe>(),
+                ModContent.ItemType<ExperimentalNanoPickaxeCreative>(),
+                ModContent.ItemType<ExperimentalNanoPickaxeXenium>(),
+            });
 
-			RecipeGroup.RegisterGroup("IndustrialPickaxes:IndustrialNano", group);
-		}
-	}
+            RecipeGroup.RegisterGroup("IndustrialPickaxes:IndustrialNano", group);
+        }
+
+        public override void ModifyInterfaceLayers(List<GameInterfaceLayer> layers)
+        {
+            Player player = Main.LocalPlayer;
+            if (player.HeldItem.type == ItemType("MasterManipulator"))
+            {
+                int index = layers.FindIndex(layer => layer.Name.Equals("Vanilla: Ruler"));
+                //layers.RemoveAt(index);
+                LegacyGameInterfaceLayer CrossHair = new LegacyGameInterfaceLayer("IndustrialPickaxes: Master Manipulator",
+                    delegate
+                    {
+                        DrawMasterManipulator(Main.spriteBatch);
+                        return true;
+                    },
+                    InterfaceScaleType.Game);
+                layers.Insert(index, CrossHair);
+            }
+        }
+
+        public void DrawMasterManipulator(SpriteBatch spriteBatch)
+        {
+            Player player = Main.LocalPlayer;
+            Texture2D texture = GetTexture("Items/ManipulatorUI/MasterUI");
+            Point uiPositionPoint = Main.MouseWorld.ToTileCoordinates();
+            Vector2 uiPosition = uiPositionPoint.ToVector2();
+            float distance = Vector2.Distance(player.Center, Main.MouseWorld);
+            if (distance < 160 && player.active && !player.dead)
+            {
+                spriteBatch.Draw(texture, uiPosition * 16 + new Vector2(8, 8) - Main.screenPosition, null, Color.White * 0.2f, 0, new Vector2(texture.Width / 2, texture.Height / 2), 1f, SpriteEffects.None, 0f);
+            }
+        }
+    }
 }
