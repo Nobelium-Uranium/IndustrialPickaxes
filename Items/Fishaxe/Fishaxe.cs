3ad2226fe8e63d3a7866fe7d9989ba6604ba5685
Chem <minecartanimation@gmail.com>
2020-10-21 05:08:44 -0600

    Various adjustments

--- Items/Fishaxe/Fishaxe.cs
+++ Items/Fishaxe/Fishaxe.cs
@@ -53,19 +53,23 @@ namespace IndustrialPickaxes.Items.Fishaxe
 			for (int i = 0; i < Main.maxItems; i++)
 			{
 				Item item = Main.item[i];
-                Item item2 = Main.item[i];
-
+                
                 if (item.active && item.type == ModContent.ItemType<EclipsiumBar>())
 				{
-                    if (item2.active && item2.type == ModContent.ItemType<IndustrialSingularity>())
+                    for (int o = 0; i < Main.maxItems; i++)
                     {
-                        if (base.item.getRect().Intersects(item.getRect()) && base.item.getRect().Intersects(item2.getRect()) && base.item.lavaWet == item.lavaWet == item2.lavaWet == true && Main.eclipse)
-                        {
-                            Item.NewItem(base.item.getRect(), ModContent.ItemType<AscendedFishaxe>());
-                            Main.PlaySound(SoundID.Item119, base.item.position);
+                        Item item2 = Main.item[o];
 
-                            base.item.active = false;
-                            item.active = false;
+                        if (item2.active && item2.type == ModContent.ItemType<IndustrialSingularity>())
+                        {
+                            if (base.item.getRect().Intersects(item.getRect()) && base.item.getRect().Intersects(item2.getRect()) && base.item.lavaWet == item.lavaWet == item2.lavaWet == true && Main.eclipse)
+                            {
+                                Item.NewItem(base.item.getRect(), ModContent.ItemType<AscendedFishaxe>());
+                                Main.PlaySound(SoundID.Item119, base.item.position);
+
+                                base.item.active = false;
+                                item.active = false;
+                            }
                         }
                     }
 				}
